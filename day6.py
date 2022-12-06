f9c6bba9159fde10a3a37d39110276f847cdd9d8
Tomás Machado <104420450+tomas-miro@users.noreply.github.com>
2022-12-06 13:50:03 +0100

    Cleaned up just for for Jelmer 💪

--- day6.py
+++ day6.py
@@ -1,25 +1,20 @@
-
-from collections import Counter
-
-inputs = ["mjqjpqmgbljsphdztnvjfqwrcgsmlb",
-        "bvwbjplbgvbhsrlpgdmjqwftvncz",
-"nppdvjthqldpwncqszvftbrmjlhg",
-"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 
-"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"]
-ex_sols = [7, 5,6,10,11]
+# inputs = ["mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+#         "bvwbjplbgvbhsrlpgdmjqwftvncz",
+# "nppdvjthqldpwncqszvftbrmjlhg",
+# "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 
+# "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"]
+# ex_sols = [7,5,6,10,11]
 
 inputs = open('./inputs/day6.txt', 'r').read().split('\n')
-
 
 sols = []
 #cut_off = 4 #1st part
 cut_off = 14 #2nd part
 
-for i in inputs:
+for i in inputs:  
     for l in range(len(i)):
         packet = i[l:l+cut_off]
         if len(set(packet)) == cut_off:
-            print(packet)
             sols.append(l+cut_off)
             break
 print(sols)
